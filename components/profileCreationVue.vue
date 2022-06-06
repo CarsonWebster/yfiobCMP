e76ee843fa6864033c026717d7cb433c0b553824
CarsonWebster <CarsonWebster@hotmail.com>
2022-06-05 20:19:56 -0700

    freeze profile creation refresh

--- components/profileCreationVue.vue
+++ components/profileCreationVue.vue
@@ -49,7 +49,7 @@ async function uploadProfile() {
             </div> -->
             <div class="w-auto h-auto flex justify-center items-center border-2 rounded-md">
                 <div class="block p-6 rounded-lg shadow-lg bg-white max-w-md">
-                    <form>
+                    <!-- <form> -->
                         <div class="form-group mb-6">
                             <input type="test" class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="accountCreationFormUsername" placeholder="Username" v-model="username">
                         </div>
@@ -94,7 +94,7 @@ async function uploadProfile() {
                         <button @click='uploadProfile' type="submit" class=" w-full px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Sign up</button>
                         <!-- <button @click="logInfo">LogInfo</button>
                         <button @click="authStore.loadUserProfile">loadUserProfile</button> -->
-                    </form>
+                    <!-- </form> -->
                 </div>
             </div>
         </main>
