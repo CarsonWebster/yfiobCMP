f1d4efd7af1514ea819fd6c94963d1232c1877c1
CarsonWebster <CarsonWebster@hotmail.com>
2022-06-07 11:15:24 -0700

    enabled profile update form reload

--- components/profileUpdateVue.vue
+++ components/profileUpdateVue.vue
@@ -37,7 +37,7 @@ async function updateProfile() {
 
 <template>
     <div>
-        <!-- <form> -->
+        <form>
                     <div class="form-group mb-6">
                         <input type="test" class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="accountCreationFormUsername" :placeholder="profile.username" v-model="username">
                     </div>
@@ -82,6 +82,6 @@ async function updateProfile() {
                     <button @click='updateProfile' type="submit" class=" w-full px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Update Information</button>
                     <!-- <button @click="logInfo">LogInfo</button>
                     <button @click="authStore.loadUserProfile">loadUserProfile</button> -->
-                <!-- </form> -->
+                </form>
     </div>
 </template>
