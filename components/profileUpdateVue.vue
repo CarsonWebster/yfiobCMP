988d95e992e6f4db87e8b7395b40cd405c1a6ce0
CarsonWebster <CarsonWebster@hotmail.com>
2022-06-07 11:21:54 -0700

    typo

--- components/profileUpdateVue.vue
+++ components/profileUpdateVue.vue
@@ -39,7 +39,7 @@ async function updateProfile() {
     <div>
         <form>
                     <div class="form-group mb-6">
-                        <input type="test" class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="accountCreationFormUsername" :placeholder="profile.username" v-model="username">
+                        <input type="text" class="form-control block w-full px-3 py-1.5 text-base font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="accountCreationFormUsername" :placeholder="profile.username" v-model="username">
                     </div>
                     <div class="grid grid-cols-2 gap-4">
                         <div class="form-group mb-6">
