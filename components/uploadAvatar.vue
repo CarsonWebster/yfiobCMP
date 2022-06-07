caabe0e35458b3f7127e8c6d049d5acabefae46c
CarsonWebster <CarsonWebster@hotmail.com>
2022-06-07 11:24:24 -0700

    upload avatar form enable

--- components/uploadAvatar.vue
+++ components/uploadAvatar.vue
@@ -98,7 +98,7 @@ async function uploadAvatar() {
           disabled={uploading}
         />
       </div> -->
-      <!-- <form> -->
+      <form>
         <input
             type="file"
             id="single"
@@ -106,6 +106,6 @@ async function uploadAvatar() {
             @change='updateAvatar'
             />
             <button @click='uploadAvatar' type="submit" class="w-auto px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">Update Avatar</button>
-        <!-- </form> -->
+        </form>
     </div>
 </template>
