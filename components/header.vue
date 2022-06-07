fb931ef52bb43b3991c4033568bd2faa5b528526
CarsonWebster <CarsonWebster@hotmail.com>
2022-06-07 15:10:49 -0700

    header home fix

--- components/header.vue
+++ components/header.vue
@@ -16,7 +16,7 @@ async function fullLogout() {
 <div>
     <header class="p-4 bg-white rounded-lg shadow md:px-6 md:py-8 dark:bg-gray-800">
     <div class="sm:flex sm:items-center sm:justify-between">
-        <a href="home" class="flex items-center mb-4 sm:mb-0">
+        <a href="/" class="flex items-center mb-4 sm:mb-0">
             <img src="@/assets/images/yfiobtran.png" class="mr-3 h-8" alt="YFIOB Logo" />
             <!-- <p class="mr-4 hover:underline md:mr-6 flex flex-wrap items-center mb-6 text-sm text-gray-500 sm:mb-0 dark:text-gray-400">Home</p> -->
             <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white"></span>
